3c20572b1193da981110981243066b0f14a719be
Marglodis Tovar <marglodistovar@gmail.com>
2025-02-22 12:37:31 -0300

    Estructura inicial del proyecto

---
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: false,
+  templateUrl: './basic-page.component.html',
+  styles: ``
+})
+export class BasicPageComponent {
+
+}
---
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: false,
+  templateUrl: './switches-page.component.html',
+  styles: ``
+})
+export class SwitchesPageComponent {
+
+}
