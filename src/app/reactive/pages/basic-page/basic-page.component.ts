7449617a42c1de4a83ecd90514617fb30680348e
Marglodis Tovar <marglodistovar@gmail.com>
2025-02-23 16:59:15 -0300

    mensajes de validación

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,23 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-
 const rtx5090 = {
   name: 'RTX-5090',
   price: 2500,
-  inStorage:7
-
-}
+  inStorage: 7,
+};
 
 @Component({
   standalone: false,
   templateUrl: './basic-page.component.html',
   styles: ``,
 })
-export class BasicPageComponent implements OnInit{
-  
-
-  
+export class BasicPageComponent implements OnInit {
   //Una forma de declarar un formulario reactivo
   /*   public myForm: FormGroup = new FormGroup({
     name: new FormControl(''),
@@ -30,21 +25,44 @@ export class BasicPageComponent implements OnInit{
 
   constructor(private formBuilder: FormBuilder) {
     this.myForm = this.formBuilder.group({
-      name: ['',[Validators.required,Validators.minLength(3)]],
-      price: [0,[Validators.required, Validators.min(0)]],
-      inStorage: [0,[Validators.required,Validators.min(0)]],
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      price: [0, [Validators.required, Validators.min(0)]],
+      inStorage: [0, [Validators.required, Validators.min(0)]],
     });
   }
   ngOnInit(): void {
     // Initialize any necessary data or perform any required setup
-    this.myForm.reset(rtx5090);
+    //this.myForm.reset(rtx5090);
     console.log('BasicPageComponent initialized');
   }
 
-  onSave():void{
-    if(this.myForm.invalid) return;
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido.';
+
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracteres`;
+      }
+    }
+    return null;
+  }
+
+  onSave(): void {
+    if (this.myForm.invalid) return;
     console.log(this.myForm.value);
 
-    this.myForm.reset({price:0, inStorage:0});
+    this.myForm.reset({ price: 0, inStorage: 0 });
   }
 }
