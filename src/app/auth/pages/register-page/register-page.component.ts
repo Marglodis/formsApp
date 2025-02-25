e7548f71aea09bd598392452c332d193431a31d9
Marglodis Tovar <marglodistovar@gmail.com>
2025-02-24 23:15:55 -0300

    Validar forms arrays

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   standalone: false,
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class RegisterPageComponent {
 
+  public myForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      name:['',[Validators.required]],
+      email:['',[Validators.required]],
+      username:['',[Validators.required]],
+      password:['',[Validators.required, Validators.minLength(6)]],
+      password2:['',[Validators.required]],
+    });
+    
+  }
+
+  onSubmit(){
+    
+  }
 }
--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   standalone: false,
   templateUrl: './switches-page.component.html',
-  styles: ``
+  styles: ``,
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
+  public myForm!: FormGroup;
+  public person = {
+    gender: 'F',
+    wantNotifications: false,
+  };
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      gender: ['M', Validators.required],
+      wantNotifications: [true, Validators.required],
+      termsAndConditions: [false, Validators.requiredTrue],
+    });
+  }
+  ngOnInit(): void {
+    this.myForm.reset(this.person);
+  }
 
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  onSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+
+      return;
+    }
+    const { termsAndConditions, ...newPerson } = this.myForm.value;
+    this.person = newPerson;
+
+    console.log(this.myForm.value);
+    console.log(this.person);
+  }
 }
